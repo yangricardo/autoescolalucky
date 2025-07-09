c69945eabd8abeba1f42ab83b95ba7722dc4b921
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-07-09 01:43:50 -0300

    feat: add GitHub Actions workflow for deploying to GitHub Pages and update project configuration

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/autoescolalucky/', // Replace with your actual repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false, // Disable sourcemaps for production
+  },
 })
