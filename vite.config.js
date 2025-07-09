fd79c2d824e3cec7eb8ddac443de327dbc58daeb
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-07-09 01:49:46 -0300

    fix: comment out SPA routing redirect and update base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/autoescolalucky/', // Replace with your actual repository name
+  base: '/', // Replace with your actual repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
