47df5393caf8cb944a92b5a6d8bc929d04be3bf7
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-08-05 23:10:33 -0300

    feat: refactor SocialLink component to use props from socialLinks data structure and add target and rel attributes

---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,16 @@
+import { SocialLinkProps } from "../data/socialLinks";
+import { SocialIcon } from "./SocialIcon";
+
+export function SocialLink(props: Readonly<SocialLinkProps>) {
+  return (
+    <a
+      href={props.href}
+      target='_blank'
+      rel='noopener noreferrer'
+      aria-label={props['aria-label']}
+      className='text-gray-500 hover:text-blue-600 transition-colors'
+    >
+      <SocialIcon path={props.path} />
+    </a>
+  );
+}
