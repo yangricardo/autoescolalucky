869903ef565ca027a95c6b73075bc95c1737149e
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-08-06 01:03:48 -0300

    fix: enhance ServiceCard component with hover effect for icon scaling

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -10,8 +10,8 @@ export const ServiceCard = ({
   title,
   children,
 }: ServiceCardProps) => (
-  <div className="bg-blue-800 text-white p-8 rounded-xl shadow-lg text-center">
-    <div className="text-5xl text-yellow-300 mb-4 mx-auto w-fit">
+  <div className="bg-blue-800 text-white p-8 rounded-xl shadow-lg text-center group hover:shadow-amber-200 hover:shadow-lg transition-shadow duration-300">
+    <div className="text-5xl text-yellow-300 mb-4 mx-auto w-fit transition-transform duration-300 group-hover:scale-125">
       {icon}
     </div>
     <h3 className="text-2xl font-bold mb-3">{title}</h3>
