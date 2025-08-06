fb5662906b0a99162ec7dfa5c2ab9b56b220b2cd
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-08-05 23:21:32 -0300

    feat: add NavLink component for creating styled navigation links

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from 'react';
+
+type NavLinkProps = {
+  href: string;
+};
+
+const NavLink = ({ href, children }: PropsWithChildren<NavLinkProps>) => (
+  <a
+    href={href}
+    className="font-semibold text-lucky-blue hover:text-blue-900 transition duration-300"
+  >
+    {children}
+  </a>
+);
+
+export default NavLink;
