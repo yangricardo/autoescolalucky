d5ef638440dd88ba832fdf34ca1eedb96a8c79a1
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-08-05 23:50:51 -0300

    feat: refactor App component layout and restore main content structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { Footer } from "./components/Footer";
 import { SEOHead } from "./components/SEOHead";
 import { Header } from "./components/Header";
-import { ContactSection } from "./components/ContactSection";
+import { Contact } from "./components/Contact";
+import { Hero } from "./components/Hero";
+import { Promotions } from "./components/Promotions";
+import { Services } from "./components/Services";
+import { Training } from "./components/Training";
 
-function App() {
+function App ()
+{
   return (
     <>
       <SEOHead />
-      <div className='min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col select-none'>
-        {/* Main Content */}
-        <main className='flex-1 flex items-center justify-center px-4 py-4'>
-          <div className='max-w-md w-full text-center space-y-4'>
-            <Header />
-            <ContactSection />
-          </div>
-        </main>
-
-        <Footer />
-      </div>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <Promotions />
+        <Training />
+        <Contact />
+      </main>
+      <Footer />
     </>
   );
 }
