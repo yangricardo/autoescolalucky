08c3f234eac0bf67b02e723d8ba9ea1de0c3fafe
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2025-08-05 23:56:12 -0300

    fix: reorder import statements in main.tsx for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { initGA } from "./utils/analytics.ts";
+import "./index.css";
 
 initGA();
 
